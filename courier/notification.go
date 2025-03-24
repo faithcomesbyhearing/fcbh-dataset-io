c0bdea9e326aabc8ee85ae59ea381388ee302446
gary <gary@shortsands.com>
2025-03-24 13:14:25 -0400

    Notification is working.

--- courier/notification.go
+++ courier/notification.go
@@ -7,6 +7,7 @@ import (
 	"github.com/aws/aws-sdk-go/service/s3"
 	req "github.com/faithcomesbyhearing/fcbh-dataset-io/decode_yaml/request"
 	log "github.com/faithcomesbyhearing/fcbh-dataset-io/logger"
+	"path/filepath"
 	"strings"
 	"testing"
 	"time"
@@ -15,20 +16,21 @@ import (
 func (b *Courier) Notification(req req.Request, status *log.Status, duration time.Duration) *log.Status {
 	var st *log.Status
 	if !testing.Testing() || b.IsUnitTest {
+		var recipients []string
 		var subject string
 		var message string
-		var recipients []string
+		var attachments []string
 		if status == nil {
+			recipients = req.NotifyOk
 			subject = "SUCCESS: " + b.dataset
 			message = b.successMsg(duration)
-			recipients = req.NotifyOk
+			attachments = b.GetOutputByExt(".html")
 		} else {
+			recipients = req.NotifyErr
 			subject = "FAILED: " + b.dataset
 			message = b.failureMsg(status, duration)
-			recipients = req.NotifyErr
 		}
-		//st = SendMessage(b.ctx, recipients, subject, message)
-		st = SendEmail(b.ctx, recipients, subject, message)
+		st = GoMailSendMail(b.ctx, recipients, subject, message, attachments)
 	}
 	return st
 }
@@ -36,27 +38,31 @@ func (b *Courier) Notification(req req.Request, status *log.Status, duration tim
 func (b *Courier) failureMsg(status *log.Status, duration time.Duration) string {
 	var message []string
 	message = append(message, "FAILED: "+b.dataset)
-	message = append(message, status.Message)
-	message = append(message, "Duration: "+duration.String())
-	message = append(message, status.Trace)
-	message = append(message, status.Request)
-	return strings.Join(message, "\n")
+	message = append(message, "Message: "+status.Message)
+	message = append(message, "Duration: "+duration.Round(100*time.Millisecond).String())
+	message = append(message, "Stack Trace: "+status.Trace)
+	message = append(message, "Request: "+status.Request)
+	return strings.Join(message, "\n\n")
 }
 
 func (b *Courier) successMsg(duration time.Duration) string {
 	var message []string
 	message = append(message, "SUCCESS: "+b.dataset)
-	message = append(message, "Duration: "+duration.String())
-	s3Client, status := b.preSignedURLClient()
-	if status == nil {
-		for _, output := range b.outputs {
-			message = append(message, output)
-			key := b.createKey(b.run, "output", output)
-			signedURL := b.genLongPreSignedURL(s3Client, key)
-			message = append(message, signedURL)
-		}
+	message = append(message, "Duration: "+duration.Round(100*time.Millisecond).String())
+	message = append(message, "Output: ")
+	files := b.GetOutputByExt(".html")
+	for _, file := range files {
+		message = append(message, "\t"+filepath.Base(file)+"\n")
 	}
-	return strings.Join(message, "\n")
+	//s3Client, status := b.preSignedURLClient()
+	//if status == nil {
+	//	for _, output := range b.outputs {
+	//		message = append(message, output)
+	//		key := b.createKey(b.run, "output", output)
+	//		signedURL := b.genLongPreSignedURL(s3Client, key)
+	//		message = append(message, signedURL)
+	//	}
+	return strings.Join(message, "\n\n")
 }
 
 func (b *Courier) preSignedURLClient() (*s3.S3, *log.Status) {
--- go.mod
+++ go.mod
@@ -5,11 +5,13 @@ go 1.23.0
 toolchain go1.23.3
 
 require (
+	github.com/aws/aws-sdk-go v1.38.20
 	github.com/aws/aws-sdk-go-v2 v1.35.0
 	github.com/aws/aws-sdk-go-v2/config v1.29.3
 	github.com/aws/aws-sdk-go-v2/service/s3 v1.63.0
 	github.com/aws/aws-sdk-go-v2/service/ses v1.29.1
 	github.com/divan/num2words v1.0.1
+	github.com/go-gomail/gomail v0.0.0-20160411212932-81ebce5c23df
 	github.com/go-sql-driver/mysql v1.8.1
 	github.com/mattn/go-sqlite3 v1.14.24
 	github.com/sergi/go-diff v1.3.1
@@ -22,7 +24,6 @@ require (
 
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
-	github.com/aws/aws-sdk-go v1.38.20 // indirect
 	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.6.5 // indirect
 	github.com/aws/aws-sdk-go-v2/credentials v1.17.56 // indirect
 	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.16.26 // indirect
@@ -50,5 +51,7 @@ require (
 	golang.org/x/exp v0.0.0-20240416160154-fe59bbe5cc7f // indirect
 	golang.org/x/image v0.18.0 // indirect
 	golang.org/x/net v0.29.0 // indirect
+	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
 	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
+	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df // indirect
 )
