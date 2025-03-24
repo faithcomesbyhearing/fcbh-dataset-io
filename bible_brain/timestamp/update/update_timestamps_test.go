0ac2f29fc7af0eed25ea092a9f498b8854388753
gary <gary@shortsands.com>
2025-03-24 13:55:46 -0400

    Notification is working.

--- bible_brain/timestamp/update/update_timestamps_test.go
+++ bible_brain/timestamp/update/update_timestamps_test.go
@@ -13,8 +13,29 @@ func TestUpdateTimestamps_Process(t *testing.T) {
 	ctx := context.Background()
 	req := request.Request{
 		UpdateDBP: request.UpdateDBP{
-			Timestamps: []string{"ENGESVN1DA", "ENGESVN1DA-opus16", "ENGESVN1SA", "ENGESVN2DA",
-				"ENGESVN2DA-opus16", "ENGESVN2SA"},
+			Timestamps: []string{"ENGESVN1DA", "ENGESVN1SA", "ENGESVN2DA", "ENGESVN2SA"},
+		},
+	}
+	conn := db.NewDBAdapter(ctx, "test_data/ENGESVN1DA_TS.db")
+	update := NewUpdateTimestamps(ctx, req, conn)
+	status := update.Process()
+	if status != nil {
+		t.Fatal(status)
+	}
+}
+
+// The following have no timestamps:
+// AAAMLTN1DA_TS.yaml
+// ABIWBTN1DA_TS.yaml
+// ABPWBTN1DA_TS.yaml
+// ACCIBSN1DA_TS.yaml
+func TestInsertTimestamps_Process(t *testing.T) {
+	log.SetOutput("stdout")
+	ctx := context.Background()
+	req := request.Request{
+		UpdateDBP: request.UpdateDBP{
+			Timestamps: []string{"AAAMLTN1DA", "AAAMLTN1DA-opus16", "AAAMLTN2DA",
+				"AAAMLTN2DA-opus16"},
 		},
 	}
 	conn := db.NewDBAdapter(ctx, "test_data/ENGESVN1DA_TS.db")
