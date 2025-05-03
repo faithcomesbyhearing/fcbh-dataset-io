7e7453dd0f9d99d35f9a1dfe563949983de58703
gary <gary@shortsands.com>
2025-05-03 16:26:38 -0400

    Progress on zero shot.

--- mms/zero_shot/build_env.sh
+++ mms/zero_shot/build_env.sh
@@ -11,3 +11,16 @@ conda activate mms_zero_shot
 pip install flashlight-text
 pip install git+https://github.com/kpu/kenlm.git
 pip install cmake
+pip install nltk
+
+conda install -y boost -c conda-forge
+conda install -y cmake -c conda-forge
+
+git clone https://github.com/kpu/kenlm
+cd kenlm
+mkdir -p build
+cd build
+cmake ..
+make -j 4
+
+
