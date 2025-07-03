185790e79f4ba8aacde2acab56c7e98068c4c6d0
gary <gary@shortsands.com>
2025-07-03 15:04:34 -0400

    Update controller to process adapter_asr as well as mms_asr.

--- mms/mms_asr/mms_asr.py
+++ mms/mms_asr/mms_asr.py
@@ -37,7 +37,7 @@ if adapter:
     outputDir = os.path.join(os.getenv('FCBH_DATASET_DB'), 'mms_adapters', lang)
     adapterFile = WAV2VEC2_ADAPTER_SAFE_FILE.format(lang)
     adapterFile = os.path.join(outputDir, adapterFile)
-    adapterWeights = safe_load_file(adapter_file)
+    adapterWeights = safe_load_file(adapterFile)
     model.load_adapter(adapterWeights, lang)
     processorDir = os.path.join(outputDir, "processor_" + lang)
     processor = Wav2Vec2Processor.from_pretrained(processorDir)
