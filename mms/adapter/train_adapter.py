e9aae78bca8efdac584ef6b8a05089e2808d75c2
gary <gary@shortsands.com>
2025-07-14 02:13:21 -0400

    Removed commented dead code, and added trainer.py to be developed.

--- mms/adapter/train_adapter.py
+++ mms/adapter/train_adapter.py
@@ -10,12 +10,10 @@ from tokenizer import createTokenizer
 from sqlite_utility import *
 from data_preparation import *
 from dataset import *
-from mms_collator import *
 from evaluate import load
 from safetensors.torch import save_file as safe_save_file
 from transformers.models.wav2vec2.modeling_wav2vec2 import WAV2VEC2_ADAPTER_SAFE_FILE
 from memory_callback import *
-from sampler import *
 from torch.utils.data import DataLoader
 
 #
