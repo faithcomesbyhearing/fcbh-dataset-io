648190a557034aa1581ba030bbc877c5d53bba7c
gary <gary@shortsands.com>
2025-07-26 11:24:57 -0400

    Escape spaces in directory name.

--- mms/adapter/train_adapter.go
+++ mms/adapter/train_adapter.go
@@ -12,7 +12,6 @@ import (
 	"os"
 	"path/filepath"
 	"strconv"
-	"strings"
 )
 
 type TrainAdapter struct {
@@ -49,16 +48,17 @@ func (t *TrainAdapter) Train(files []input.InputFile) *log.Status {
 	}
 	pythonPath := os.Getenv(`FCBH_MMS_ADAPTER_PYTHON`)
 	pythonScript := filepath.Join(os.Getenv("GOPROJ"), "mms/adapter/trainer.py")
-	replacer := strings.NewReplacer(
-		" ", "\\ ",
-		"(", "\\(",
-		")", "\\)",
-	)
+	//replacer := strings.NewReplacer(
+	//	" ", "\\ ",
+	//	"(", "\\(",
+	//	")", "\\)",
+	//)
 	status := stdio_exec.RunScriptWithLogging(t.ctx, pythonPath, pythonScript,
 		t.langISO,
 		t.conn.DatabasePath,
 		//strings.Replace(tempDir, ` `, `\ `, -1),
-		replacer.Replace(tempDir),
+		//replacer.Replace(tempDir),
+		`'`+tempDir+`'`,
 		strconv.Itoa(t.args.BatchMB),
 		strconv.Itoa(t.args.NumEpochs),
 		strconv.FormatFloat(t.args.LearningRate, 'e', -1, 64),
