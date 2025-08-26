3789d7e874b1afa02b8be6a24f4ffc7a62fb7ac3
gary <gary@shortsands.com>
2025-08-26 11:56:45 -0600

    wav2vec2 sqlite_utility is unit tested.

---
+++ wav2vec2/train/sqlite_utility.py
@@ -0,0 +1,63 @@
+# SqliteUtility.py
+#
+# This program provides convenience methods wrapping the Sqlite3 client.
+# This class supports the same interface as SQLUtility whenever possible
+#
+
+import os
+import sys
+import sqlite3
+
+class SqliteUtility:
+
+    def __init__(self, databasePath):
+        self.conn = sqlite3.connect(databasePath)
+
+    def close(self):
+        if self.conn != None:
+            self.conn.close()
+            self.conn = None
+
+    def selectOne(self, statement, values):
+        cursor = self.conn.cursor()
+        try:
+            cursor.execute(statement, values)
+            result = cursor.fetchone()
+            cursor.close()
+            return result
+        except Exception as err:
+            self.error(cursor, statement, err)
+
+    def select(self, statement, values):
+        cursor = self.conn.cursor()
+        try:
+            cursor.execute(statement, values)
+            resultSet = cursor.fetchall()
+            cursor.close()
+            return resultSet
+        except Exception as err:
+            self.error(cursor, statement, err)
+
+    def execute(self, statement, values):
+        cursor = self.conn.cursor()
+        try:
+            cursor.execute(statement, values)
+            cursor.close()
+            self.conn.commit()
+        except Exception as err:
+            self.error(cursor, statement, err)
+
+    def error(self, cursor, stmt, error):
+        cursor.close()
+        print("ERROR executing SQL %s on '%s'" % (error, stmt))
+        self.conn.rollback()
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    sql = SqliteUtility(os.getenv("FCBH_DATASET_DB") + "/GaryNTest/N2ENGWEB.db")
+    count = sql.selectOne("select count(*) from scripts",())
+    print(count)
+    (audioFile, text) = sql.selectOne('select audio_file, script_text from scripts where script_id = ?', (10,))
+    print(audioFile, text)
+    sql.close()
