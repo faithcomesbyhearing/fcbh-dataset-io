639e775bba0ae35c1358f6a3f5c0c4ebeccaa8fc
Jon Stearley <jrstear@fcbhmail.org>
2025-10-23 09:35:52 -0600

    fix test 3a by removing hard coding

--- tests/3a_zip_file_test.go
+++ tests/3a_zip_file_test.go
@@ -1,26 +1,38 @@
 package tests
 
 import (
+	"fmt"
+	"os"
+	"path/filepath"
 	"testing"
 )
 
-const zipFile = `is_new: yes
+func TestZipFileDirect(t *testing.T) {
+	// Build paths using GOPROJ environment variable for portability
+	goproj := os.Getenv("GOPROJ")
+	if goproj == "" {
+		t.Skip("GOPROJ environment variable not set - run 'source setup_env.sh' first")
+	}
+
+	audioZip := filepath.Join(goproj, "input/ENGWEB/ENGWEBN2DA/ENGWEBN2DA.zip")
+	textFiles := filepath.Join(goproj, "input/ENGWEB/ENGWEBN_ET-usx/*.usx")
+
+	zipFileYaml := fmt.Sprintf(`is_new: yes
 dataset_name: 3a_zip_file
 bible_id: ENGWEB
 username: GaryNTest
 output:
   sqlite: yes
 audio_data:
-  file: /Users/gary/FCBH2024/download/ENGWEBN2DA-mp3-64.zip
+  file: %s
 text_data:
-  file: /Users/gary/FCBH2024/download/ENGWEBN_ET-usx.zip
+  file: %s
 testament:
   nt_books: [MRK]
-`
+`, audioZip, textFiles)
 
-func TestZipFileDirect(t *testing.T) {
 	var tests []SqliteTest
 	tests = append(tests, SqliteTest{"SELECT count(*) FROM scripts", 694})
 	tests = append(tests, SqliteTest{"SELECT count(*) FROM scripts WHERE script_begin_ts != 0.0", 0})
-	DirectSqlTest(zipFile, tests, t)
+	DirectSqlTest(zipFileYaml, tests, t)
 }
