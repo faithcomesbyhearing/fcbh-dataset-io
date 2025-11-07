73a0942def961e45389264681179e9b6ce4a0bcb
Gary N Griswold <gary@shortsands.com>
2025-11-07 00:27:55 -0500

    Work on computing silence.

--- match/research/silence/silence_analysis.go
+++ match/research/silence/silence_analysis.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
 	"github.com/faithcomesbyhearing/fcbh-dataset-io/db"
 	"github.com/faithcomesbyhearing/fcbh-dataset-io/decode_yaml/request"
@@ -14,6 +15,39 @@ import (
 	"strconv"
 )
 
+type SilenceRec struct {
+	ScriptId    int64
+	BookId      string
+	ChapterNum  int
+	VerseStr    string
+	ScriptEndTS float64
+	WordId      int64
+	WordSeq     int
+	BeginTS     float64
+	EndTS       float64
+	Duration    float64
+	Silence     float64
+}
+
+func (s *SilenceRec) Ref() string {
+	return s.BookId + " " + strconv.Itoa(s.ChapterNum) + ":" + s.VerseStr + "." + strconv.Itoa(s.WordSeq)
+}
+func (s *SilenceRec) BeginT() string {
+	return strconv.FormatFloat(s.BeginTS, 'f', 2, 64)
+}
+func (s *SilenceRec) EndT() string {
+	return strconv.FormatFloat(s.EndTS, 'f', 2, 64)
+}
+func (s *SilenceRec) EndScript() string {
+	return strconv.FormatFloat(s.ScriptEndTS, 'f', 2, 64)
+}
+func (s *SilenceRec) Dur() string {
+	return strconv.FormatFloat(s.Duration, 'f', 2, 64)
+}
+func (s *SilenceRec) Silen() string {
+	return strconv.FormatFloat(s.Silence, 'f', 2, 64)
+}
+
 func main() {
 	ctx := context.Background()
 	log.SetOutput("stderr")
@@ -22,52 +56,7 @@ func main() {
 	if status != nil {
 		os.Exit(1)
 	}
-	type SilenceRec struct {
-		ScriptId int64
-		WordId   int64
-		BeginTS  float64
-		EndTS    float64
-		Duration float64
-		Silence  float64
-	}
-	var query = `SELECT w.script_id, w.word_id, w.word_begin_ts, w.word_end_ts, s.script_end_ts
-		FROM scripts s JOIN words w ON s.script_id = w.script_id
-		WHERE w.ttype = 'W'`
-	rows, err := conn.DB.Query(query)
-	if err != nil {
-		status = log.Error(ctx, 500, err, "Error in SQL query of silence")
-		os.Exit(1)
-	}
-	defer rows.Close()
-	var results []SilenceRec
-	var scriptEndTS float64
-	var priorEndTS float64
-	var priorScriptEndTS float64
-	for rows.Next() {
-		var s SilenceRec
-		err = rows.Scan(&s.ScriptId, &s.WordId, &s.BeginTS, &s.EndTS, &scriptEndTS)
-		if err != nil {
-			status = log.Error(ctx, 500, err, `Error scanning in Select Silence`)
-			os.Exit(1)
-		}
-		if len(results) == 0 {
-			s.Silence = s.BeginTS
-		} else {
-			s.Silence = s.BeginTS - priorEndTS
-			if s.Silence < 0 {
-				s.Silence = priorScriptEndTS - priorEndTS + s.BeginTS
-			}
-		}
-		s.Duration = s.EndTS - s.BeginTS
-		priorEndTS = s.EndTS
-		priorScriptEndTS = scriptEndTS
-		results = append(results, s)
-	}
-	err = rows.Err()
-	if err != nil {
-		status = log.Error(ctx, 500, err, `Error at end of rows in Silence`)
-		os.Exit(1)
-	}
+	results := selectSilences(ctx, nil, conn)
 	var silences []float64
 	for _, result := range results {
 		silences = append(silences, result.Silence)
@@ -75,7 +64,10 @@ func main() {
 			fmt.Println(result.Silence, result.ScriptId, result.WordId)
 		}
 	}
-	compute(silences, "silence", "N2MZJSIM", "")
+	compute(silences, "silence", "N2MZJSIM", "./match/research/silence")
+	//findMaxSilenceInRefs(ctx, conn)
+	findWorstVerses(results)
+	//dumpTimestamps(results)
 }
 
 func compute(floats []float64, desc string, stockNo string, directory string) {
@@ -85,6 +77,7 @@ func compute(floats []float64, desc string, stockNo string, directory string) {
 		panic(err)
 	}
 	defer file.Close()
+	log.Info(context.Background(), "Output filename", filename)
 	mean := stat.Mean(floats, nil)
 	write(file, "Mean: ", strconv.FormatFloat(mean, 'f', 2, 64))
 	stdDev := stat.StdDev(floats, nil)
@@ -154,19 +147,126 @@ func write(file *os.File, args ...string) {
 	_, _ = file.WriteString("\n")
 }
 
-// select script_id, word_id, timestamps
-// compute duration and silence
-// silence of the first is begin of zeroth
-// copy in compute
+type ref struct {
+	bookId     string
+	chapterNum int
+	verseStr   string
+}
 
-/*
+func findMaxSilenceInRefs(ctx context.Context, conn db.DBAdapter) {
+	var refs []ref
+	refs = append(refs, ref{bookId: "MAT", chapterNum: 5, verseStr: "36"})
+	refs = append(refs, ref{bookId: "MAT", chapterNum: 8, verseStr: "9"})
+	refs = append(refs, ref{bookId: "MAT", chapterNum: 12, verseStr: "33"})
+	refs = append(refs, ref{bookId: "MAT", chapterNum: 12, verseStr: "37"})
+	refs = append(refs, ref{bookId: "MAT", chapterNum: 17, verseStr: "22"}) //"22-23"})
+	refs = append(refs, ref{bookId: "MAT", chapterNum: 22, verseStr: "32"})
+	refs = append(refs, ref{bookId: "MAT", chapterNum: 25, verseStr: "38"})
+	refs = append(refs, ref{bookId: "MAT", chapterNum: 26, verseStr: "6"}) //"6-7"})
+	refs = append(refs, ref{bookId: "MRK", chapterNum: 4, verseStr: "5"})
+	refs = append(refs, ref{bookId: "MRK", chapterNum: 4, verseStr: "24"})
+	refs = append(refs, ref{bookId: "MRK", chapterNum: 9, verseStr: "39"})
+	refs = append(refs, ref{bookId: "MRK", chapterNum: 10, verseStr: "38"})
+	refs = append(refs, ref{bookId: "LUK", chapterNum: 4, verseStr: "19"})
+	refs = append(refs, ref{bookId: "LUK", chapterNum: 11, verseStr: "42"})
+	refs = append(refs, ref{bookId: "LUK", chapterNum: 20, verseStr: "38"})
+	refs = append(refs, ref{bookId: "LUK", chapterNum: 23, verseStr: "29"})
+	refs = append(refs, ref{bookId: "JHN", chapterNum: 3, verseStr: "21"})
+	refs = append(refs, ref{bookId: "JHN", chapterNum: 5, verseStr: "22"}) //"22-23"})
+	refs = append(refs, ref{bookId: "JHN", chapterNum: 19, verseStr: "6"})
+	refs = append(refs, ref{bookId: "JHN", chapterNum: 21, verseStr: "19"})
+	refs = append(refs, ref{bookId: "ACT", chapterNum: 8, verseStr: "39"})
+	refs = append(refs, ref{bookId: "ACT", chapterNum: 12, verseStr: "10"})
+	refs = append(refs, ref{bookId: "EPH", chapterNum: 3, verseStr: "6"})
+	for _, r := range refs {
+		silence := findMaxSilence(ctx, &r, conn)
+		log.Info(ctx, r.bookId, r.chapterNum, r.verseStr, silence)
+	}
+}
 
-	var chars []generic.AlignChar
-	var query = `SELECT s.audio_file, s.script_id, s.book_id, s.chapter_num, s.verse_str,
-				w.word_id, w.word, c.char_id, c.seq, c.uroman, c.start_ts, c.end_ts, c.fa_score
-				FROM scripts s JOIN words w ON s.script_id = w.script_id
-				JOIN chars c ON w.word_id = c.word_id
-				WHERE w.ttype = 'W'
-				ORDER BY c.char_id`
-	rows, err := d.DB.Query(query)
-*/
+func findMaxSilence(ctx context.Context, ref *ref, conn db.DBAdapter) float64 {
+	results := selectSilences(ctx, ref, conn)
+	log.Info(ctx, "---")
+	log.Info(ctx, ref.bookId, ref.chapterNum, ref.verseStr)
+	for _, res := range results {
+		log.Info(ctx, "res", res.Silence)
+	}
+	var maxi = 0.0
+	for i, item := range results {
+		if i > 0 && item.Silence > maxi { // first one must is incorrect.
+			maxi = item.Silence
+		}
+	}
+	log.Info(ctx, "maxi", maxi)
+	return maxi
+}
+
+func selectSilences(ctx context.Context, ref *ref, conn db.DBAdapter) []SilenceRec {
+	var query = `SELECT w.script_id, s.book_id, s.chapter_num, s.verse_str, s.script_end_ts,
+		w.word_id, w.word_seq, w.word_begin_ts, w.word_end_ts
+		FROM scripts s JOIN words w ON s.script_id = w.script_id
+		WHERE w.ttype = 'W'`
+	var rows *sql.Rows
+	var err error
+	if ref == nil {
+		query += " ORDER BY w.word_id"
+		rows, err = conn.DB.Query(query)
+	} else {
+		query += " AND s.book_id = ? AND s.chapter_num = ? AND s.verse_str = ? ORDER BY w.word_id"
+		rows, err = conn.DB.Query(query, ref.bookId, ref.chapterNum, ref.verseStr)
+	}
+	if err != nil {
+		_ = log.Error(ctx, 500, err, "Error in SQL query of silence")
+		os.Exit(1)
+	}
+	defer rows.Close()
+	var results []SilenceRec
+	for rows.Next() {
+		var s SilenceRec
+		err = rows.Scan(&s.ScriptId, &s.BookId, &s.ChapterNum, &s.VerseStr, &s.ScriptEndTS,
+			&s.WordId, &s.WordSeq, &s.BeginTS, &s.EndTS)
+		if err != nil {
+			_ = log.Error(ctx, 500, err, `Error scanning in Select Silence`)
+			os.Exit(1)
+		}
+		s.Duration = s.EndTS - s.BeginTS
+		results = append(results, s)
+	}
+	err = rows.Err()
+	if err != nil {
+		_ = log.Error(ctx, 500, err, `Error at end of rows in Silence`)
+		os.Exit(1)
+	}
+	results[0].Silence = results[0].BeginTS
+	for i := 1; i < len(results)-1; i++ {
+		if results[i].BookId != results[i+1].BookId || results[i].ChapterNum != results[i+1].ChapterNum {
+			results[i].Silence = results[i].ScriptEndTS - results[i-1].BeginTS
+		} else {
+			results[i].Silence = results[i].BeginTS - results[i-1].EndTS
+		}
+	}
+	last := len(results) - 1
+	results[last].Silence = results[last].ScriptEndTS - results[last-1].BeginTS
+	return results
+}
+
+func findWorstVerses(silences []SilenceRec) {
+	sort.Slice(silences, func(i, j int) bool {
+		return silences[i].Silence > silences[j].Silence
+	})
+	silences = silences[0:1000]
+	sort.Slice(silences, func(i, j int) bool {
+		return silences[i].WordId < silences[j].WordId
+	})
+	for _, s := range silences {
+		fmt.Println(s.Ref(), s.ScriptId, s.WordId, s.Silence)
+	}
+}
+
+func dumpTimestamps(silences []SilenceRec) {
+	//for _, s := range silences {
+	for i := 0; i < 5000; i++ {
+		s := silences[i]
+		fmt.Println(s.Ref(), s.BeginT(), s.EndT(), s.EndScript(), s.Dur(), s.Silen())
+	}
+}
