c2eb87bbd99c8c19791d4614d433f387cd8e9733
Gary N Griswold <gary@shortsands.com>
2025-11-11 19:15:00 -0500

    mms_asr2 is working.

--- mms/mms_asr2/mms_asr2_test.go
+++ mms/mms_asr2/mms_asr2_test.go
@@ -7,17 +7,19 @@ import (
 	"github.com/faithcomesbyhearing/fcbh-dataset-io/decode_yaml/request"
 	"github.com/faithcomesbyhearing/fcbh-dataset-io/input"
 	log "github.com/faithcomesbyhearing/fcbh-dataset-io/logger"
+	"github.com/faithcomesbyhearing/fcbh-dataset-io/utility/stdio_exec"
+	"github.com/faithcomesbyhearing/fcbh-dataset-io/utility/uroman"
 	"os"
 	"path"
+	"strconv"
 	"testing"
 )
 
 func TestMMSASR2_ProcessFiles(t *testing.T) {
 	ctx := context.Background()
 	log.SetOutput("stderr")
-	//conn := db.NewDBAdapter(ctx, ":memory:")
 	user := request.GetTestUser()
-	conn, status := db.NewerDBAdapter(ctx, true, user, "N2MZJSIM")
+	conn, status := db.NewerDBAdapter(ctx, false, user, "N2MZJSIM") // is not used
 	asr := NewMMSASR2(ctx, conn, "mzj", "", false)
 	var files []input.InputFile
 	var file input.InputFile
@@ -32,4 +34,37 @@ func TestMMSASR2_ProcessFiles(t *testing.T) {
 	if status != nil {
 		t.Fatal(status)
 	}
+}
+
+func TestMMSASR2_ParseResult(t *testing.T) {
+	ctx := context.Background()
+	log.SetOutput("stderr")
+	user := request.GetTestUser()
+	conn, status := db.NewerDBAdapter(ctx, false, user, "N2MZJSIM")
+	if status != nil {
+		t.Fatal(status)
+	}
+	asr := NewMMSASR2(ctx, conn, "mzj", "", false)
+	var file input.InputFile
+	file.BookId = "MAT"
+	file.Chapter = 12
+	file.MediaId = "N2MZJSIM"
+	asr.uroman, status = stdio_exec.NewStdioExec(asr.ctx, os.Getenv(`FCBH_MMS_FA_PYTHON`), uroman.ScriptPath(), "-l", asr.lang)
+	if status != nil {
+		t.Fatal(status)
+	}
+	defer func() {
+		status = asr.uroman.Close()
+	}()
+	response := readResultFile(file)
+	status = asr.parseResult(file, response)
+}
+
+func readResultFile(file input.InputFile) string {
+	content, err := os.ReadFile(file.MediaId + "_" + file.BookId + strconv.Itoa(file.Chapter))
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	return string(content)
 }
