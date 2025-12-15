de2cbc87b5f342ccb74274d381024b38937e2b33
Gary N Griswold <gary@shortsands.com>
2025-12-15 18:17:28 -0500

    Added systemctl in controller package

--- EC2_start_script.sh
+++ EC2_start_script.sh
@@ -1,5 +1,4 @@
 #!/bin/bash -v
-
 runuser --login ec2-user --shell=/bin/bash << 'EOF'
 env
 cd ~/go/src/fcbh-dataset-io
@@ -9,7 +8,9 @@ else
     git pull origin main
 fi
 go install ./controller/queue_server
+
 cd
 nohup ~/go/bin/queue_server &
+
 EOF
-exit 0
+#exit 0
