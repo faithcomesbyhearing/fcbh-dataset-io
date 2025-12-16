57614c063fb5aea298c53bdf6dc2bb1778d49a15
Gary N Griswold <gary@shortsands.com>
2025-12-15 19:08:41 -0500

    Added systemctl in controller package

---
+++ controller/systemctl/arti-start-script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+
+# Ensure HOME is set (systemd may not set it)
+export HOME=/home/ec2-user
+
+# Change to the application directory
+cd /home/ec2-user/go/src/fcbh-dataset-io
+
+# Pull latest code based on environment
+if [[ "$FCBH_DATASET_QUEUE" == *"-dev" ]]; then
+    git pull origin main
+else
+    git pull origin main
+fi
+
+# Build and install the application
+go install ./controller/queue_server
+
+# Run the queue_server directly
+exec /home/ec2-user/go/bin/queue_server
